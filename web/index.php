68b4479d25c8e2561c819fbedd61256f255e047c
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-19 14:58:48 +0200

    Attempting breakpoint use for font size

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@
     <body style="background: #EFE7DA; height:100%; position: relative; margin:0;">
 
         <div style="margin: 0; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%); max-width:40em; padding-top: 2em; padding-bottom:2em; padding-right:2em; padding-left: 50px; color: white; background: black; z-index: 2;">
-            <div class="rh-caption" style="line-height:1.4; font-family: arial, sans-serif; color:white;">Den 20 augusti lanserar vi på Musik Hallandia vår nya webbplats, så kom tillbaka då så får du ta del av både den och höstens programutbud.</div>
+            <div style="line-height:1.4; font-family: arial, sans-serif; color:white;"><h1 class="rh-caption" style="font-weight: normal;">Den 20 augusti lanserar vi på Musik Hallandia vår nya webbplats, så kom tillbaka då så får du ta del av både den och höstens programutbud.</h1></div>
 
             <span style="width:48px; position: absolute; top:40px; left: -24px; border-bottom:3px solid white;"></span>
             <span style="width:48px; position: absolute; top:48px; left: -24px; border-bottom:3px solid white;"></span>
