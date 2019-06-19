4c6886496fa0c3973b6ad8a65c2c0181ebfddb87
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-19 14:11:02 +0200

    First attempt at content and layout. Take it for testdrive in different devices

--- web/index.php
+++ web/index.php
@@ -1,7 +1,23 @@
 <html>
-<head>
-</head>
-<body>
-Välkommen till musikhallandia.se!
-</body>
+    <head>
+        <title>
+            Musik Hallandia
+        </title>
+    </head>
+    <body style="background: #EFE7DA; height:100%; position: relative; margin:0;">
+
+        <div style="margin: 0; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%); max-width:40em; padding-top: 2em; padding-bottom:2em; padding-right:2em; padding-left: 50px; color: white; background: black; z-index: 2;">
+            <div style="line-height:1.4; font-family: arial, sans-serif; font-size: 180%; color:white;">Den 20 augusti lanserar vi på Musik Hallandia vår nya webbplats, så kom tillbaka då så får du ta del av både den och höstens programutbud.</div>
+
+            <span style="width:48px; position: absolute; top:40px; left: -24px; border-bottom:3px solid white;"></span>
+            <span style="width:48px; position: absolute; top:48px; left: -24px; border-bottom:3px solid white;"></span>
+            <span style="width:48px; position: absolute; top:56px; left: -24px; border-bottom:3px solid white;"></span>
+            <span style="width:48px; position: absolute; top:64px; left: -24px; border-bottom:3px solid white;"></span>
+            <span style="width:48px; position: absolute; top:72px; left: -24px; border-bottom:3px solid white;"></span>
+
+        </div>
+        <span style="z-index:1;width:0; height: 0; position: absolute; bottom: 0; left: 0;  border-left: 130px solid #FA3CB4; border-top: 230px solid transparent;"></span>
+        <span style="z-index:1;width:0; height: 0; position: absolute; top: 0; right: 0; border-right: 150px solid #FCF800; border-bottom: 320px solid transparent;"></span>
+
+    </body>
 </html>
